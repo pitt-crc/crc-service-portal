67935e6cabf44ecd8515e759de18b2c940711b1a
Daniel Perrefort <djperrefort@gmail.com>
2024-10-28 20:49:07 -0400

    Adds endpoint for file attachments (#467)
    * Adds attachment endpoint
    
    * Updates openapi

--- keystone_api/apps/allocations/urls.py
+++ keystone_api/apps/allocations/urls.py
@@ -7,8 +7,9 @@ from .views import *
 app_name = 'allocations'
 
 router = DefaultRouter()
-router.register(r'clusters', ClusterViewSet)
 router.register(r'allocations', AllocationViewSet)
+router.register(r'attachments', AttachmentViewSet)
+router.register(r'clusters', ClusterViewSet)
 router.register(r'requests', AllocationRequestViewSet)
 router.register(r'reviews', AllocationRequestReviewViewSet)
 
