1b7c9acc051296d2f913e374713400bae05617aa
Daniel Perrefort <djperrefort@gmail.com>
2024-11-06 12:58:02 -0500

    Increase default API rate limits (#473)

--- keystone_api/main/settings.py
+++ keystone_api/main/settings.py
@@ -170,8 +170,8 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle'
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'anon': env.str('API_THROTTLE_ANON', '1000/day'),
-        'user': env.str('API_THROTTLE_USER', '10000/day')
+        'anon': env.str('API_THROTTLE_ANON', '120/min'),
+        'user': env.str('API_THROTTLE_USER', '240/min')
     },
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
