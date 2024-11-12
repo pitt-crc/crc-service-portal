b093ffa3b49ce8548b35df66f259830dbe36e1d5
Daniel Perrefort <djperrefort@gmail.com>
2024-11-12 11:07:12 -0500

    Drops MEDIA_URL setting

--- keystone_api/main/settings.py
+++ keystone_api/main/settings.py
@@ -288,7 +288,6 @@ STATIC_URL = 'static/'
 STATIC_ROOT = Path(env.path('CONFIG_STATIC_DIR', BASE_DIR / 'static_files'))
 STATIC_ROOT.mkdir(parents=True, exist_ok=True)
 
-MEDIA_URL = 'media/'
 MEDIA_ROOT = Path(env.path('CONFIG_UPLOAD_DIR', BASE_DIR / 'media'))
 MEDIA_ROOT.mkdir(parents=True, exist_ok=True)
 
